1a157ffededb4c9c90c90cc433bf4fde4eb49ece
Johan <johan225@hotmail.es>
2019-11-05 17:23:11 -0500

    Agrega funcionalidad repetir

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+          repeat: ""
+		}
+	}
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" onChange={this.repeater.bind(this)} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.repeat}</p>
       </div>
     );
+  }
+  
+  repeater(event){
+    this.setState({
+			repeat: event.target.value
+	  });
   }
 }
 
